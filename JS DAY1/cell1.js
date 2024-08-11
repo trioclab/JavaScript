7363b0140b2da18b18ec6e133dd64aae60acabbf
Trioclab <Saniakiran085@gmail.com>
2024-08-11 23:57:52 +0500

    Second Day of learning JS

--- JS DAY1/cell1.js
+++ JS DAY1/cell1.js
@@ -5,8 +5,37 @@ console.log("This is the first thing I learn in the JS.")
 alert("Your website is Hacked now 🤓")
 
 // Mathematical Operations in JS
+
+// Sum of the numbers
 const num1 = 12;
 const num2 = 97;
-// Sum of the numbers
 const sum = num1 + num2 ;
 console.log(sum);
+
+// Substraction of numbers
+const num01 = 89;
+const num02 = 80;
+const sub = num02 - num01;
+console.log(sub);
+
+// Multiplication 
+const Multiply = num1 * num2;
+console.log(Multiply);
+
+// Division 
+const Division = num1 / 2 ;
+console.log(Division);
+
+// Reminder
+const Reminder = 17 % 2;
+console.log(Reminder);
+
+// Equality and if else statment in JS 
+if(num1 === 12){
+    console.log("num1 is strictly equal to number 12.")
+}else if(num1 == 12){
+    console.log("num1 is equal to 12.")
+}else{
+    console.log("They are not equal duh!")
+}
+
---
+++ JS DAY1/cell2.js
@@ -0,0 +1,8 @@
+// Loops in JavaScript 
+// 1: For loop 
+
+let country = "Pakistan";
+for ( let i = 0 ; i >= 100 ; i++ ){
+    console.log(country);
+}
+
