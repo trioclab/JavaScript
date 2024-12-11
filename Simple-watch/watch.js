b565897b715a36c3bf341fbb4211e9b9246e77f2
Trioclab <Saniakiran085@gmail.com>
2024-12-11 12:42:23 +0500

    Add a watch simple

--- Simple-watch/watch.js
+++ Simple-watch/watch.js
@@ -5,8 +5,28 @@ const watch = ()=>{
     const sec = String(time.getSeconds()).padStart(2 , '0')
     
     document.getElementById('watch').textContent = `${hours}:${min}:${sec}`
+    document.addEventListener('mousemove', (e) => {
+        const sparkle = document.getElementsByClassName('warpper');
+        sparkle.className = 'sparkle';
+        document.body.appendChild(sparkle);
+      
+        gsap.set(sparkle, {
+          x: e.pageX,
+          y: e.pageY,
+          scale: gsap.utils.random(0.5, 1.5),
+        });
+      
+        gsap.to(sparkle, {
+          opacity: 0,
+          scale: 2,
+          duration: 1,
+          onComplete: () => sparkle.remove(),
+        });
+      });
 }
 
 setInterval(watch , 1000);
 
 watch()
+
+
